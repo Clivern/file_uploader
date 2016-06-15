4634d06f88cb2195d1a4813ac43d26704d394b88
clivern <hello@clivern.com>
2016-06-15 23:45:01 +0200

    validator class added

--- src/Uploader.php
+++ src/Uploader.php
@@ -2,7 +2,12 @@
 
 namespace Clivern\FileUploader;
 
-class Uploader
-{
+/**
+ * Uploader Class
+ */
+class Uploader {
 
+    public function __construct($configs){}
+    public function uploadFile($input_name, $validation_rules){}
+    public function getErrors(){}
 }
--- src/Validator.php
+++ src/Validator.php
@@ -2,7 +2,110 @@
 
 namespace Clivern\FileUploader;
 
-class Validator
-{
+/**
+ * Validator Class
+ */
+class Validator {
 
+    /**
+     * A list of catched errors
+     *
+     * @since 1.0.0
+     * @access private
+     * @var array
+     */
+    private $errors = [];
+
+    /**
+     * A list of validation rules
+     *
+     * @since 1.0.0
+     * @access private
+     * @var array
+     */
+    private $roles;
+
+    /**
+     * Class constructor
+     *
+     * @since 1.0.0
+     * @access public
+     * @param boolean $supported_extensions
+     * @param boolean $supported_types
+     * @param string  $max_size
+     */
+    public function __construct($supported_extensions = false, $supported_types = false, $max_size = "32MB")
+    {
+        $this->roles['supported_extensions'] = $supported_extensions;
+        $this->roles['supported_types'] = $supported_types;
+        $this->roles['max_size'] = $max_size;
+    }
+
+    /**
+     * Validate uploaded file
+     *
+     * @since 1.0.0
+     * @access public
+     * @param array $file_info
+     * @return boolean
+     */
+    public function validate($file_info)
+    {
+        if( (isset($file_info['error'])) && ($file_info['error'] > 0) ){
+            $this->errors[] = "Error while uploading the file";
+        }
+
+        if( (is_array($this->roles['supported_extensions'])) && !(in_array($file_info['extension'], $this->roles['supported_extensions'])) ) ){
+            $this->errors[] = "File extension is invalid.";
+        }
+
+        if( (is_array($this->roles['supported_types'])) && !(in_array($file_info['type'], $this->roles['supported_types'])) ){
+            $this->errors[] = "File type is invalid.";
+        }
+
+        $max_size = $this->updateSize($this->roles['max_size']);
+
+        if( ($this->roles['max_size'] != false) && ($file_info['size'] > $max_size) ){
+            $this->errors[] = "File size must not exceed {$this->roles['max_size']}.";
+        }
+
+        return ( count($this->errors) > 0 ) ? false : true;
+    }
+
+    /**
+     * Get Upload Errors
+     *
+     * @since 1.0.0
+     * @access public
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * Update size to bytes
+     *
+     * @since 1.0.0
+     * @access private
+     * @param string $size
+     * @return mixed
+     */
+    private function updateSize($size)
+    {
+
+        if( strpos($size, "KB") !== false){
+            return intval($size) * 1;
+        }elseif( strpos($size, "MB") !== false){
+            return intval($size) * 1;
+        }elseif( strpos($size, "GB") !== false){
+            return intval($size) * 1;
+        }elseif( strpos($size, "TB") !== false){
+            return intval($size) * 1;
+        }else{
+            return false
+        }
+
+    }
 }
