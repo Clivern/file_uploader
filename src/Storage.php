235e14580aca1d9ab888a176c35c046c248d6855
clivern <hello@clivern.com>
2016-06-15 23:06:58 +0200

    storage class docs added

--- src/Storage.php
+++ src/Storage.php
@@ -2,14 +2,58 @@
 
 namespace Clivern\FileUploader;
 
-class Storage
-{
+/**
+ * Storage Class
+ */
+class Storage {
+
+    /**
+     * Storage dir name
+     *
+     * @since 1.0.0
+     * @access private
+     * @var string
+     */
     private $dir_name;
+
+    /**
+     * Storage dir path
+     *
+     * @since 1.0.0
+     * @access private
+     * @var string
+     */
     private $dir_path;
+
+    /**
+     * Whether storage is year based
+     *
+     * @since 1.0.0
+     * @access private
+     * @var boolean
+     */
     private $year_storage_based;
+
+    /**
+     * Whether storage is month based
+     *
+     * @since 1.0.0
+     * @access private
+     * @var boolean
+     */
     private $month_storage_based;
 
-    public funtion setDir($dir_path, $dir_name, $year_storage_based = true, $month_storage_based = true)
+    /**
+     * Class constructor
+     *
+     * @since 1.0.0
+     * @access public
+     * @param string $dir_path
+     * @param string $dir_name
+     * @param boolean $year_storage_based
+     * @param boolean $month_storage_based
+     */
+    public funtion __construct($dir_path, $dir_name, $year_storage_based = true, $month_storage_based = true)
     {
         $this->dir_path = rtrim($dir_path, '/') . '/';
         $this->dir_name = $dir_name;
@@ -17,25 +61,79 @@ class Storage
         $this->month_storage_based = $month_storage_based;
     }
 
+    /**
+     * Build Storage Folders
+     *
+     * @since 1.0.0
+     * @access public
+     * @return boolean
+     */
     public buildStorage()
     {
 
+        $base_storage = $this->buildBaseDir();
+        if( !$base_storage ){
+            throw new \Exception('Error while creating ' . this->dir_path . $this->dir_name);
+        }
+
+        $year_based_storage = ($this->year_storage_based) ? $this->buildYearDir() : true;
+        if( !$year_based_storage ){
+            throw new \Exception('Error while creating ' . $this->dir_path . $this->dir_name . "/" . date("Y"));
+        }
+
+        $month_based_storage = ($this->year_storage_based && $this->month_storage_based) ? $this->buildMonthDir() : true;
+        if( !$month_based_storage ){
+            throw new \Exception('Error while creating ' . $this->dir_path . $this->dir_name . "/" . date("Y") . "/" . date("m"));
+        }
+
+        return true;
     }
 
+    /**
+     * Get Upload Path
+     *
+     * @since 1.0.0
+     * @access public
+     * @return string
+     */
     public funtion getUploadPath()
     {
+        $path = $this->dir_path . $this->dir_name;
 
+        if( $this->year_storage_based ){
+            $path .= "/" . date("Y");
+        }
+
+        if( $this->year_storage_based && $this->month_storage_based ){
+            $path .= "/" . date("m");
+        }
+
+        return $path;
     }
 
-    public funtion buildBaseDir()
+    /**
+     * Buid base dir if not exist
+     *
+     * @since 1.0.0
+     * @access private
+     * @return boolean
+     */
+    private funtion buildBaseDir()
     {
-        if( !$this->dirExists() ){
+        if( !$this->baseDirExists() ){
             return (boolean) @mkdir($this->dir_path . $this->dir_name);
         }
         return true;
     }
 
-    public funtion buildYearDir()
+    /**
+     * Build year dir if not exist
+     *
+     * @since 1.0.0
+     * @access private
+     * @return boolean
+     */
+    private funtion buildYearDir()
     {
         if( !$this->currentYearDirExists() ){
             return (boolean) @mkdir($this->dir_path . $this->dir_name . "/" . date("Y"));
@@ -43,7 +141,14 @@ class Storage
         return true;
     }
 
-    public funtion buildMonthDir()
+    /**
+     * Build month dir if not exist
+     *
+     * @since 1.0.0
+     * @access private
+     * @return boolean
+     */
+    private funtion buildMonthDir()
     {
         if( !$this->currentMonthDirExists() ){
             return (boolean) @mkdir($this->dir_path . $this->dir_name . "/" . date("Y") . "/" . date("m"));
@@ -51,17 +156,38 @@ class Storage
         return true;
     }
 
-    public funtion dirExists()
+    /**
+     * Check if base dir exist
+     *
+     * @since 1.0.0
+     * @access private
+     * @return boolean
+     */
+    private funtion baseDirExists()
     {
         return (boolean) ( is_dir($this->dir_path . $this->dir_name) );
     }
 
-    public funtion currentYearDirExists()
+    /**
+     * Check if current year dir exist
+     *
+     * @since 1.0.0
+     * @access private
+     * @return boolean
+     */
+    private funtion currentYearDirExists()
     {
         return (boolean) ( is_dir($this->dir_path . $this->dir_name . "/" . date("Y")) );
     }
 
-    public funtion currentMonthDirExists()
+    /**
+     * Check if current month dir exist
+     *
+     * @since 1.0.0
+     * @access private
+     * @return boolean
+     */
+    private funtion currentMonthDirExists()
     {
         return (boolean) ( is_dir($this->dir_path . $this->dir_name . "/" . date("Y") . "/" . date("m")) );
     }
